c939342787a42676be1d84432b3258d7abcb04af
GuTaeho <sbpdt1062@gmail.com>
2025-02-15 23:11:25 +0900

    Fix: 빌드 에러 수정

--- Sources/SoyBeanUI/Extension/UIView+Font.swift
+++ Sources/SoyBeanUI/Extension/UIView+Font.swift
@@ -17,7 +17,7 @@ public extension UIView {
 public extension View {
     /// Note: Xcode 프리뷰에서 커스텀 폰트를 표시하기 위해 호출할 것, 앱에서는 필요하지 않음
     func loadCustomFonts() -> some View {
-        Font.registerFonts()
+        UIFont.registerFonts()
         return self
     }
 }
