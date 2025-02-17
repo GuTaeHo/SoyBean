807fcf4202c48b96a886ba8a085e01826c1ba5d7
gutaeho_macbook_air <sbpdt1062@gmail.com>
2025-02-17 10:05:33 +0900

    Refactor: Font os 분기 수정(SwiftUI 동시 사용 가능)

--- Sources/SoyBeanUI/Extension/UIFont+Register.swift
+++ Sources/SoyBeanUI/Extension/UIFont+Register.swift
@@ -5,8 +5,6 @@
 //  Created by 구태호 on 2/15/25.
 //
 
-#if os(iOS)
-import UIKit
 import SwiftUI
 
 public enum FontType: String, CaseIterable {
@@ -16,7 +14,21 @@ public enum FontType: String, CaseIterable {
     case bold700 = "Pretendard-Bold"
 }
 
+#if os(iOS)
+import UIKit
+
 public extension UIFont {
+    static func custom(type: FontType, size: CGFloat) -> UIFont {
+        return UIFont(name: type.rawValue, size: size) ?? .systemFont(ofSize: size)
+    }
+}
+
+#endif
+
+public extension Font {
+    static func custom(_ type: FontType, size: CGFloat) -> Font {
+        return .custom(type.rawValue, fixedSize: size)
+    }
     /// 폰트를 등록합니다.
     /// - Returns: 등록된 폰트 이름 배열 반환
     /// - Important: 앱 진입점에서 호출
@@ -31,17 +43,4 @@ public extension UIFont {
             }
         }
     }
-    
-    static func custom(type: FontType, size: CGFloat) -> UIFont {
-        return UIFont(name: type.rawValue, size: size) ?? .systemFont(ofSize: size)
-    }
 }
-
-public extension Font {
-    static func custom(_ type: FontType, size: CGFloat) -> Font {
-        return .custom(type.rawValue, fixedSize: size)
-    }
-}
-
-#elseif os(macOS)
-#endif
