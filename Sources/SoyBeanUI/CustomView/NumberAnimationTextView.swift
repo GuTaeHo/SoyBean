b40e69bd5a7e0456911f5280fa2fd6155ae4e71d
gutaeho_macbook_air <sbpdt1062@gmail.com>
2025-02-21 16:18:33 +0900

    Feat: NumberAnimationTextView 추가

---
+++ Sources/SoyBeanUI/CustomView/NumberAnimationTextView.swift
@@ -0,0 +1,34 @@
+//
+//  NumberAnimationTextView.swift
+//  SoyBean
+//
+//  Created by 구태호 on 2/21/25.
+//
+
+import SwiftUI
+
+
+/// 애니메이션이 적용된 TextView
+/// - Note: iOS 16 이상부터 애니메이션 적용
+public struct NumberAnimationTextView: View {
+    @Binding var number: Int
+    
+    public var body: some View {
+        if #available(iOS 16.0, macOS 13.0, *) {
+            numberText
+                .contentTransition(.numericText())
+        } else {
+            numberText
+        }
+    }
+    
+    @ViewBuilder
+    var numberText: some View {
+        Text("\(number)")
+            .font(.custom(.bold700, size: 30))
+    }
+}
+
+#Preview {
+    NumberAnimationTextView(number: .constant(1010))
+}
