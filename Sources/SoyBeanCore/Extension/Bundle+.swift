b0784e0bbabb34531016d9e2e51ed1c77fd04d0e
gutaeho_macbook_air <sbpdt1062@gmail.com>
2025-03-12 10:59:53 +0900

    Refactor: Bundle.appInstallEnvironment 수정

--- Sources/SoyBeanCore/Extension/Bundle+.swift
+++ Sources/SoyBeanCore/Extension/Bundle+.swift
@@ -9,17 +9,28 @@ import Foundation
 
 
 public extension Bundle {
-    enum AppConfiguration {
-        case debug
+    enum AppInstallEnvironment {
+        case direct
         case testFlight
         case appStore
+        
+        var name: String {
+            switch self {
+            case .direct:
+                return "직접설치"
+            case .testFlight:
+                return "테스트플라이트"
+            case .appStore:
+                return "앱스토어"
+            }
+        }
     }
     
     /// 앱 설치 환경 반환
     /// - Important: `Mac Catalyst` 를 지원하는 앱에서는 동작하지않음
-    var appInstallEnvironment: AppConfiguration {
+    var appInstallEnvironment: AppInstallEnvironment {
         #if DEBUG
-        return .debug
+        return .direct
         #else
         if Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt" {
             return .testFlight
